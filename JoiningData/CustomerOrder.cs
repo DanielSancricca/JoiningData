c7f49a9c4faecf92b21cd7eca062fe571b24d007
Daniel Sancricca <dansancricca@gmail.com>
2022-05-09 08:55:54 -0400

    first commit for joining data lab

---
+++ JoiningData/CustomerOrder.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JoiningData
+{
+    class CustomerOrder
+    {
+        public string CustomerName { get; set; }
+        public string Item { get; set; }
+        public float Price { get; set; }
+        public int Quantity { get; set; }
+
+        public CustomerOrder(string customerName, string item, float price, int quantity)
+        {
+            this.CustomerName = customerName;
+            this.Item = item;
+            this.Price = price;
+            this.Quantity = quantity;
+        }
+    }
+}
---
+++ JoiningData/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace JoiningData
+{
+    public class Program
+    {
+        
+        public static void Main()
+        {
+            List<CustomerOrder> orders = new List<CustomerOrder>();
+
+            CustomerOrder a = new CustomerOrder("Acme Hardware", "Mouse", 25, 3);
+            CustomerOrder b = new CustomerOrder("Acme Hardware", "Keyboard", 45, 2);
+            CustomerOrder c = new CustomerOrder("Falls Realty", "Macbook", 800, 2);
+            CustomerOrder d = new CustomerOrder("Julie's Morning Diner", "iPad", 525, 1);
+            CustomerOrder e = new CustomerOrder("Julie's Morning Diner", "Credit Card Reader", 45, 1);
+
+            orders.Add(a);
+            orders.Add(b);
+            orders.Add(c);
+            orders.Add(d);
+            orders.Add(e);
+            orders.ToArray();
+
+
+            foreach (CustomerOrder order in orders)
+            {
+                Console.WriteLine(order.CustomerName);
+                Console.WriteLine($"Item        Price       Quantity        ");
+                Console.WriteLine($"{order.Item}        {order.Price}         {order.Quantity}");
+                Console.WriteLine();
+            }
+
+        }   
+    }
+}
